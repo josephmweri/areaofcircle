a7cbcfef122bf0592f14d3642cadb7a29535026d
josephmweri <120680751+josephmweri@users.noreply.github.com>
2022-12-15 15:53:23 -0800

    area commit

--- app/src/main/java/com/example/areaofcircle/MainActivity.java
+++ app/src/main/java/com/example/areaofcircle/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 catch(Exception e){
+
                     Log.e("e", "invalid try again");
                 }
             }
